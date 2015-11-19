e985e846d15214e8ff19bf45ffaf70d8defd0495
insekkei <insekkei@yahoo.com>
2015-11-19 22:35:57 +0800

    修改样式及一些bug

--- app/js/services.js
+++ app/js/services.js
@@ -2,7 +2,7 @@ var bookcatServices = angular.module('bookcatServices', ['ngResource']);
 
 bookcatServices.factory('Book', ['$resource',
 	function($resource) {
-		return $resource('books/book:bookId.json', {}, {
-			query: {method: 'GET', params: {bookId: 's'}, isArray: true}
+		return $resource('books/:bookId.json', {}, {
+			query: {method: 'GET', params: {bookId: 'books'}, isArray: true}
 		});
 	}]);
